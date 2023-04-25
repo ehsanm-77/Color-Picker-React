8d090c4b97fa719e9c715eafe1aa7fec92d81592
ehsanm <ehsanmohamadi780@gmail.com>
2023-04-25 22:15:23 +0430

    change font

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,5 +1,8 @@
+import { AddRandomColor } from './layout/addRandomColor/addRandomColor';
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-blue-400">Hello world!</h1>
+    <div>
+      <AddRandomColor />
+    </div>
   );
 }
---
+++ my-project/src/layout/addRandomColor/addRandomColor.tsx
@@ -0,0 +1,3 @@
+export const AddRandomColor = () => {
+  return <h1 className="bg-green-400 flex flex-col gap-5">EMO</h1>;
+};
